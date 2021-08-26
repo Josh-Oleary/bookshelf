44ce30a37ce39c8eba136bf88680318b0eae1fac
Josh <jdoleary91@gmail.com>
2021-08-26 09:39:22 -0400

    feat: add upward data flow
    Books shelf state is now stored on both the display page and the search page

--- src/ShelfSwitcher.js
+++ src/ShelfSwitcher.js
@@ -5,18 +5,28 @@ class ShelfSwitcher extends Component {
   static propTypes = {
     changeShelf: PropTypes.func.isRequired,
     info: PropTypes.object.isRequired,
+    books: PropTypes.array.isRequired,
   }
   //function to handle change and trigger callback function passed from app
   handleChange = event => {
     this.props.changeShelf(this.props.info, event.target.value);
   }
   render(){
-    const { info } = this.props;
+    const { info, books } = this.props;
+
+    let curShelf = 'none';
+
+    //if book is on shelf, set shelf prop to info.shelf
+    for(let book of books){
+      book.id === info.id && (
+        curShelf = book.shelf
+      )
+    }
 
     return (
       <div>
         {/* sets current state option as defualt value */}
-        <select value={info.shelf || 'none'} onChange={this.handleChange} show-tick="true">
+        <select value={curShelf} onChange={this.handleChange} show-tick="true">
           <option disabled>Move to...</option>
           <option value='currentlyReading'>Reading</option>
           <option value='wantToRead'>Next Up</option>
